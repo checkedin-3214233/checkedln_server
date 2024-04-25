6bd275b0d62019af243efb0b2ac973aa404a268c
codingwithkrish <87133705+codingwithkrish@users.noreply.github.com>
2024-04-25 21:30:29 +0530

    chat feature new updates

--- controller/messageController.js
+++ controller/messageController.js
@@ -168,7 +168,7 @@ export const getChatList = async (req, res) => {
     try {
         const userId = req.user._id;
         const userChatList = await UsersChat.find({ senderId: userId }).populate("allUsers.users allUsers.lastMessage");
-        return res.status(200).json(userChatList);
+        return res.status(200).json({ "allUsers": userChatList[0].allUsers });
 
     } catch (error) {
         console.log("Error in getChatList controller: ", error.message)
