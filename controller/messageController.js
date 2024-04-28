6146ee313757d666b153ba5ad14c1a8c900919e1
codingwithkrish <87133705+codingwithkrish@users.noreply.github.com>
2024-04-28 16:11:46 +0530

    changes in get chat list

--- controller/messageController.js
+++ controller/messageController.js
@@ -169,9 +169,10 @@ export const getMessages = async (req, res) => {
 export const getChatList = async (req, res) => {
     try {
         const userId = req.user._id;
-        const userChatList = await UsersChat.find({ senderId: userId }).populate("allUsers.users allUsers.lastMessage");
-        return res.status(200).json({ "allUsers": userChatList[0].allUsers });
 
+        const userChatList = await UsersChat.findOne({ senderId: userId }).populate("allUsers.users allUsers.lastMessage");
+        if (!userChatList) return res.status(200).json({ "allUsers": [] });
+        return res.status(200).json({ "allUsers": userChatList.allUsers });
     } catch (error) {
         console.log("Error in getChatList controller: ", error.message)
         res.status(500).json({ error: "Internal Server Error" })
