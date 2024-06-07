78bf00926dbb8257e065d768daa872b81970ad0f
codingwithkrish <87133705+codingwithkrish@users.noreply.github.com>
2024-06-07 22:29:18 +0530

    event join

--- controller/updateEventController.js
+++ controller/updateEventController.js
@@ -174,3 +174,102 @@ async function notify(userName, checkInName, userToken, profileImageUrl, bannerI
 
     })
 }
+
+export const joinEventByThemSelf = async (req, res) => {
+    const { eventId } = req.params;
+    const userId = req.user._id;
+
+    try {
+        const event = await Event.findById(eventId);
+        const eventStatus = await EventStatus.findOne({ userId });
+        console.log(eventStatus);
+
+        if (!eventStatus) {
+            const newEventStatus = new EventStatus({
+                userId,
+                events: [
+                    {
+                        event: event,
+                        status: "going"
+                    }
+                ]
+            });
+            await newEventStatus.save();
+            event.attendies.push(userId);
+            await event.save();
+            const eventR = await Event.findById(event).populate("createdBy");
+            const createdUser = eventR.createdBy;
+            if (createdUser.notificationToken) {
+                await notify(req.user.name, eventR.checkInName, createdUser.notificationToken, req.user.profileImageUrl, eventR.bannerImages, eventR)
+
+            }
+            pushNotification(createdUser._id, "normal", {
+                event: eventR,
+
+                PushTitle: `${req.user.name} has joined your event ${eventR.checkInName} .`
+            }, req.user,)
+            return res.status(200).json({ message: "Event joined successfully" });
+
+        }
+        const findEventStatusById = await EventStatus.findOne({
+            userId: userId,
+            'events.event': event // Match the specific event within the events array
+        });
+        if (findEventStatusById) {
+            // Find the index of the event object within the events array that matches the eventId
+            const eventIndex = findEventStatusById.events.findIndex(eventItem => eventItem.event.equals(updatedEvent.event));
+
+            if (eventIndex === -1) {
+                throw new Error(`Event not found in the events array for user ${userId} and event ${updatedEvent.event}`);
+            }
+            findEventStatusById.events[eventIndex].status = "going";
+            await findEventStatusById.save();
+            const eventR = await Event.findById(event).populate("createdBy");
+            const createdUser = eventR.createdBy;
+            console.log(createdUser);
+            console.log(createdUser.notificationToken);
+            event.attendies.push(userId);
+            await event.save();
+            if (createdUser.notificationToken) {
+                console.log("notificationToken", createdUser.notificationToken);
+                await notify(req.user.name, eventR.checkInName, createdUser.notificationToken, req.user.profileImageUrl, eventR.bannerImages, eventR)
+
+            }
+            pushNotification(createdUser._id, "eventStatus", {
+                event: event,
+
+                PushTitle: `${req.user.name} has joined your event ${event.checkInName} .`
+            }, req.user,)
+            return res.status(200).json({ message: "Event joined successfully. You will be notified about the event in future." });
+
+        }
+        eventStatus.events.push({
+            event: updatedEvent.event,
+            status: "going"
+        });
+        await eventStatus.save();
+        event.attendies.push(userId);
+        await event.save();
+        const eventR = await Event.findById(event);
+        const createdUser = event.createdBy;
+        if (createdUser.notificationToken) {
+            console.log("notificationToken1", createdUser.notificationToken);
+
+            await notify(req.user.name, eventR.checkInName, createdUser.notificationToken, req.user.profileImageUrl, eventR.bannerImages, eventR)
+        }
+        pushNotification(createdUser._id, "normal", {
+            event: event,
+
+            PushTitle: `${req.user.name} has joined your event ${eventR.checkInName} .`
+        }, req.user,)
+        return res.status(200).json({
+            message: "Event joined successfully. You will be notified about the event in future."
+        });
+
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+
+    }
+
+}
--- route/eventRoute.js
+++ route/eventRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createEvent, getPastEvents, getUpcomingEvents, getNearByEvents, getEventById, requestEvent, changeEventStatus, acceptRequest, getBuddiesEvents, popularEvents, getLiveEvents } from '../controller/eventController.js';
-import { getShareEventLink, joinEvent, getShareEventId } from '../controller/updateEventController.js';
+import { getShareEventLink, joinEvent, getShareEventId, joinEventByThemSelf } from '../controller/updateEventController.js';
 
 const router = express.Router();
 
@@ -15,6 +15,7 @@ router.get('/request/:eventId/:status', requestEvent);
 router.get('/shareEvent/:eventId', getShareEventLink);
 router.get("/changeStatus/:eventId/:status", changeEventStatus);
 router.get('/joinEvent/:shareEventId', joinEvent);
+router.get('/joinEventByThemSelf/:eventId', joinEventByThemSelf);
 router.get('/acceptEvent/:eventId/:otherUserId', acceptRequest);
 router.get('/getShareEventId/:shareEventId', getShareEventId);
 router.get('/:eventId', getEventById);  // Ensure this is correctly placed
